9c12cff65ede674bb4d96d946ffeb20efcdb28f7
Greensych <alternative191@yandex.ru>
2025-12-26 12:40:54 +0300

    refactor: добавить XML-документацию к IDistanceService

--- src/GeoDistanceApi/Services/IDistanceService.cs
+++ src/GeoDistanceApi/Services/IDistanceService.cs
@@ -2,7 +2,17 @@ using GeoDistanceApi.Models;
 
 namespace GeoDistanceApi.Services;
 
+/// <summary>
+/// Сервис для расчёта географического расстояния.
+/// </summary>
 public interface IDistanceService
 {
+    /// <summary>
+    /// Рассчитывает расстояние между двумя координатами в километрах.
+    /// Основано на формуле Haversine.
+    /// </summary>
+    /// <param name="from">Начальная координата.</param>
+    /// <param name="to">Конечная координата.</param>
+    /// <returns>Расстояние в километрах.</returns>
     double GetDistanceKm(Coordinate from, Coordinate to);
 }
